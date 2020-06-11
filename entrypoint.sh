2c2b4c45b2ec98e34b45a3f36af49cf021983924
Volodymyr <vovochkastelmashchuk@gmail.com>
2020-06-11 23:21:00 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,3 +2,5 @@
 
 echo "Hello Word"
 echo $GITHUB_REPOSITORY
+
+curl -X POST http://goodreadme.androidstory.dev:8080/checkMe/byRepoFullName -H 'Content-Type: application/json' --data '{"fullName": "VovaStelmashchuk/Test-repo"}'
