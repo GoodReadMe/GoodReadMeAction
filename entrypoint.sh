f63bc5806aa8e463dbf8dad33a4e1c07da40accd
Volodymyr <vovochkastelmashchuk@gmail.com>
2020-06-18 23:57:09 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 echo "HOST: $1"
 echo "SECRET: $2"
-echo "Repo $GITHUB_REPOSITORY
+echo "Repo $GITHUB_REPOSITORY"
 
-curl -v -X POST "$1/checkMe/byRepoFullName" -H 'Content-Type: application/json' -H "X-CLIENT-SECRET: $2" --data '{"fullName": "$GITHUB_REPOSITORY"}'
+curl -X POST "$1/checkMe/byRepoFullName" -H "Content-Type: application/json" -H "X-CLIENT-SECRET: $2" --data '{"fullName": "$GITHUB_REPOSITORY"}'
